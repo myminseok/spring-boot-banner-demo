d1ead83e8ab091a83c09a1016d3827bda81db954
myminseok <myminseok@gmail.com>
2018-01-04 00:16:00 +0900

    init

---
+++ src/main/java/com/example/bannerdemo/BannerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.bannerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/bannerdemo/BannerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bannerdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BannerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
